d10175719e502567c402777a2e706150a803f26b
Idiatou Diallo <idiatoudiallo@admins-MacBook-Air-2.local>
2024-02-09 10:58:14 -0500

    update

--- nextjs-dashboard/scripts/seed.js
+++ nextjs-dashboard/scripts/seed.js
@@ -11,7 +11,7 @@ async function seedUsers(client) {
   try {
     await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
     // Create the "users" table if it doesn't exist
-    const createTable = await client.sql`
+    const createTable = await client.sql` 
       CREATE TABLE IF NOT EXISTS users (
         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
